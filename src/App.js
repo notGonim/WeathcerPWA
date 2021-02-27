7e292fdb837879baabd59704322730509ccb2609
notGonim <mhmodgonim96@gmail.com>
2021-02-27 18:16:36 +0200

    Done with the react part functionality

--- src/App.js
+++ src/App.js
@@ -20,7 +20,24 @@ function App() {
     <div className="main-container">
       <input type="text" className="search" placeholder="Search..."
         value={city} onChange={(e) => setCity(e.target.value)} onKeyPress={Search} />
-   
+      {weather.main &&
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description} />
+            <p>{weather.weather[0].description} </p>
+          </div>
+        </div>
+      }
     </div>
   );
 }
