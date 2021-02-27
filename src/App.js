30f5cbafbdc9eb1d6e0ad9caa6ec6610ea5de01f
notGonim <mhmodgonim96@gmail.com>
2021-02-27 18:07:22 +0200

    Added the main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { fetchWeather } from './API/FetchWether';
 
 function App() {
-  return (
-    <>
+  const [city, setCity] = useState("")
+  const [weather, setWeather] = useState({})
 
-    </>
+  const Search = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(city)
+      setWeather(data)
+      setCity('')
+
+    }
+  }
+
+
+  return (
+    <div className="main-container">
+      <input type="text" className="search" placeholder="Search..."
+        value={city} onChange={(e) => setCity(e.target.value)} onKeyPress={Search} />
+   
+    </div>
   );
 }
 
